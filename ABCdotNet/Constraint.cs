727564af4884dee960c73f77e7fd437a7ca1a77a
Xnizer <saeid.zain.aldeen@gmail.com>
2023-01-27 08:46:42 -0800

    Add a 'Constraint' struct that defines bounds for parameters in the objective function.

---
+++ ABCdotNet/Constraint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABCdotNet
+{
+    public struct Constraint
+    {
+        public double MinValue { get; }
+        public double MaxValue { get; }
+
+        public static implicit operator Constraint((double min, double max) tuple) => new Constraint(tuple.min, tuple.max);
+
+        public Constraint(double min, double max)
+        {
+            if (min > max)
+                throw new ArgumentException($"The value of '{nameof(min)}' must be greater then the value of '{nameof(max)}'.");
+
+            MinValue = min;
+            MaxValue = max;
+        }
+    }
+}
